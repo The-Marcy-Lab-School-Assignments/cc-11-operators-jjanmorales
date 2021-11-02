7f0a454be507d553fc2dec98f5678238eacdb692
Jan Morales <moralesjan40@gmail.com>
2021-11-02 14:30:37 +0000

    Completed code challenge

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 //1
-function areBothSame(){
-  
+// create two parameters
+// conditional if statement 
+// == will check only for value, not datatype
+function areBothSame(n1, n2){
+  if(n1 == n2){
+      return true;
+  }else{
+      return false;
+  }
 }
-
+console.log(areBothSame(true, 1));
 //2
-function areBothEqual(){
-  
+// create two parameters
+// conditional if statement 
+// === will check for value and datatype
+function areBothEqual(n1, n2){
+  if(n1 === n2){
+      return true;
+  }else{
+      return false;
+  }
 }
-
+console.log(areBothEqual("2", 2));
 //3
-function areAllFourEqual(){
-  
+// create four parameters
+// conditional if statement 
+// === will check for value and datatype
+// returns true equal, if not returns false
+function areAllFourEqual(n1, n2, n3, n4){
+  if( n1 === n2 && n1 === n3 && n1 === n4){
+      return true;
+  }else{
+      return false;
+  }
 }
-
+console.log(areAllFourEqual("2", "2", "2", 2));
 
 //4
-function areAllFourSame(){
-  
+// create four parameters
+// conditional if statement 
+// == will check for value only
+// returns true equal, if not returns false
+function areAllFourSame(n1, n2, n3, n4){
+  if( n1 == n2 && n1 == n3 && n1 == n4){
+      return true;
+  }else{
+      return false;
+  }
 }
-
+console.log(areAllFourSame(true, true, 1, "1"));
 //5
-function areAllFourSameOrEqual(){
-  
+// create four parameters
+// conditional if statement 
+// == will check for value only
+// === will check for value and datatype
+// returns true equal, if not returns false
+function areAllFourSameOrEqual(n1, n2, n3, n4){
+  if( n1 == n2 && n1 == n3 && n1 == n4){
+      return "same";
+  }else if( n1 == n2 && n1 == n3 && n1 == n4){
+      return "equal";
+  }else{
+      return null;
+  }
 }
-
+console.log(areAllFourSame("2", "2", "2", "2"));
 
